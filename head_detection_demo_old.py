3f3a000b606998236f18df768019e515d15c07fb
cucicov <cucicov@gmail.com>
2022-08-10 22:16:46 +0300

    finish trackinggit add .wget https://ak.picdn.net/shutterstock/videos/5699930/thumb/1.jpg finally

--- head_detection_demo_old.py
+++ head_detection_demo_old.py
@@ -21,7 +21,6 @@ from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 from tkinter import ttk
 from random import random
-import sys
 
 global prev_poses
 prev_poses = []
@@ -33,8 +32,6 @@ pos_y_min = 0
 
 res_width = 640
 res_height = 480
-
-poses_normalization_thresh = 20
 
 
 class DisplayPhotoEventHandler(FileSystemEventHandler):
@@ -69,15 +66,12 @@ class DisplayPhotoEventHandler(FileSystemEventHandler):
                 # clear canvas before drawing new circle.
                 # self.canva.delete("all")
                 # instead of clearing, draw gray circles instead of old values and draw white circles for new values
-                for pos in prev_poses:
-                    pos_x = translate(pos[0], pos_x_min, pos_x_max, 0, res_width)
-                    pos_y = translate(pos[1], pos_y_min, pos_y_max, 0, res_height)
+                for pos in prev_pos:
+                    self.canva.create_oval((pos[0] / scale) - 7, (pos[1] / scale) - 7,
+                                           ((pos[0] / scale) + 7),
+                                           ((pos[1] / scale) + 7), fill="black", outline="")
 
-                    self.canva.create_oval((pos_x / scale) - 7, (pos_y / scale) - 7,
-                                           ((pos_x / scale) + 7),
-                                           ((pos_y / scale) + 7), fill="black", outline="")
-
-                # create current_poses list
+                # normalize prev_pos with currently detected list.
                 current_poses = []
                 for i in range(pred_bboxes_.shape[0]):
                     ymin, xmin, ymax, xmax = pred_bboxes_[i, :]
@@ -91,48 +85,17 @@ class DisplayPhotoEventHandler(FileSystemEventHandler):
                 #                            ((pos[0] / scale) + 15),
                 #                            ((pos[1] / scale) + 15), fill="white")
 
-                prev_poses.sort(reverse=True)  # keep always ordered list to simulate tracking same person.
-
-                print("********")
-                print(prev_poses, current_poses)
-
-                if len(prev_poses) == 0:
-                    prev_poses = current_poses.copy()
-
-                # advanced normalization
-                if len(current_poses) > 0 and len(prev_poses) > 0:
-                    temp_prev_poses = [None] * len(current_poses)
-                    temp_current_poses = current_poses.copy()
-                    for id_p, ppos in enumerate(prev_poses):
-                        for id_c, cpos in enumerate(current_poses):
-                            x_thresh_inline = abs(ppos[0] - cpos[0]) < poses_normalization_thresh
-                            y_thresh_inline = abs(ppos[1] - cpos[1]) < poses_normalization_thresh
-                            if x_thresh_inline and y_thresh_inline:
-                                temp_prev_poses[id_c] = ppos
-                                temp_current_poses[id_c] = None
-                                break
-
-                    for idx in range(len(temp_prev_poses)):
-                        pos = temp_prev_poses[idx]
-                        if pos is None:
-                            temp_prev_poses[idx] = temp_current_poses[idx]
-
-                    prev_poses = temp_prev_poses
-
-                print(prev_poses)
-                print("********")
-
-                # new normalization of pos and current_poses
+                prev_pos.sort(reverse=True)  # keep always ordered list to simulate tracking same person.
 
                 # bring prev_pos and current_poses to same size.
-                # if len(current_poses) < len(prev_pos):
-                #     del prev_pos[pred_bboxes_.shape[0] - len(prev_pos)]
-                # elif len(current_poses) > len(prev_pos):
-                #     # add if too few elements
-                #     for i in range(len(current_poses)):
-                #         # if more elements are detected, add last in pos history
-                #         if len(prev_pos) <= i:
-                #             prev_pos.append(current_poses[i])
+                if len(current_poses) < len(prev_pos):
+                    del prev_pos[pred_bboxes_.shape[0] - len(prev_pos)]
+                elif len(current_poses) > len(prev_pos):
+                    # add if too few elements
+                    for i in range(len(current_poses)):
+                        # if more elements are detected, add last in pos history
+                        if len(prev_pos) <= i:
+                            prev_pos.append(current_poses[i])
 
                 et4 = time.time()
                 tt4 = et4 - st4
@@ -140,12 +103,15 @@ class DisplayPhotoEventHandler(FileSystemEventHandler):
 
                 print("[INFO] {} heads DETECTED.".format(str(len(current_poses))))
                 st3 = time.time()
-                for i, current_pos in enumerate(current_poses):
+                for i in range(pred_bboxes_.shape[0]):
+                    ymin, xmin, ymax, xmax = pred_bboxes_[i, :]
+                    current_pos = (int(xmin), int(ymin))
+
                     # uncomment to get position coordinates for pos_x_ and pos_y_
                     # print(current_pos)
 
-                    pos = prev_poses[i]
-                    # diff_pos = tuple(map(lambda i, j: i - j, pos, current_pos))
+                    pos = prev_pos[i]
+                    diff_pos = tuple(map(lambda i, j: i - j, pos, current_pos))
 
                     # if differences between pos and current_pos are too big, reset pos to current_pos
                     # if diff_pos[0] > 40 or diff_pos[1] > 40:  # TODO: check if same values should be here. perhaps variation is smaller on horizontal and the threshold should be bigger for that case.
@@ -154,73 +120,54 @@ class DisplayPhotoEventHandler(FileSystemEventHandler):
 
                     # while pos != current_pos:
                     st2 = time.time()
-                    # for q in range(100):  # TODO: externalize variable?
-                    #     # print("start loop")
-                    #     pos = tuple(map(lambda i, j: i - (j / 100), pos, diff_pos))
-                    #
-                    #     # pos = tuple(map(lambda i, j, k: i - (j / abs(j)) if i != k and abs(j) != 0 else i,
-                    #     #                 pos, diff_pos, current_pos))
-                    #
-                    #     # translate positions
-                    #     # pos_x = translate(pos[0], pos_x_min, pos_x_max, 0, res_width)
-                    #     # pos_y = translate(pos[1], pos_y_min, pos_y_max, 0, res_height)
-                    #     pos_x = pos[0]
-                    #     pos_y = pos[1]
-                    #
-                    #     # draw path by erasing everything underneath
-                    #     self.canva.create_oval((pos_x / scale) - 3, (pos_y / scale) - 3,
-                    #                            ((pos_x / scale) + 3),
-                    #                            ((pos_y / scale) + 2), fill="black", outline="")
-                    #     self.canva.create_oval((pos_x / scale) + random() * 2, (pos_y / scale) + random() * 2,
-                    #                            ((pos_x / scale) + 2) + random() * 2,
-                    #                            ((pos_y / scale) + 2) + random() * 2, fill="gray", outline="")
-                    #
-                    #     # TODO: check if we need this logic or just set static sleep. maybe 1/q*10?
-                    #     # print("sleep:" + str(abs(0.1 / max(diff_pos))))
-                    #     # max_diff = 1 if max(diff_pos) == 0 else max(diff_pos)
-                    #     # # print("sleep:" + str(min(abs(0.1 / max_diff), 0.05)))
-                    #     # time.sleep(min(abs(0.1 / max_diff), random() / 300))
-                    #     # time.sleep(0.001)
+                    if diff_pos[0] != 0 and diff_pos[1] != 0:
+                        for q in range(100):  # TODO: externalize variable?
+                            # print("start loop")
+                            pos = tuple(map(lambda i, j: i - (j / 100), pos, diff_pos))
 
-                    # translate positions
-                    pos_x = translate(pos[0], pos_x_min, pos_x_max, 0, res_width)
-                    pos_y = translate(pos[1], pos_y_min, pos_y_max, 0, res_height)
-                    # pos_x = pos[0]
-                    # pos_y = pos[1]  # TODO: rethink translation if we use only lines.
+                            # pos = tuple(map(lambda i, j, k: i - (j / abs(j)) if i != k and abs(j) != 0 else i,
+                            #                 pos, diff_pos, current_pos))
 
-                    # draw path by erasing everything underneath
-                    # self.canva.create_line((pos_x / scale), (pos_y / scale),
-                    #                        (current_pos[0] / scale),
-                    #                        (current_pos[1] / scale), fill="black", width=3)
-                    c_pos_x = translate(current_pos[0], pos_x_min, pos_x_max, 0, res_width)
-                    c_pos_y = translate(current_pos[1], pos_y_min, pos_y_max, 0, res_height)
-                    self.canva.create_line((pos_x / scale), (pos_y / scale),
-                                           (c_pos_x / scale),
-                                           (c_pos_y / scale), fill="gray", width=1)
+                            # translate positions
+                            # pos_x = translate(pos[0], pos_x_min, pos_x_max, 0, res_width)
+                            # pos_y = translate(pos[1], pos_y_min, pos_y_max, 0, res_height)
+                            pos_x = pos[0]
+                            pos_y = pos[1]
 
+                            # draw path by erasing everything underneath
+                            self.canva.create_oval((pos_x / scale) - 3, (pos_y / scale) - 3,
+                                                   ((pos_x / scale) + 3),
+                                                   ((pos_y / scale) + 2), fill="black", outline="")
+                            self.canva.create_oval((pos_x / scale) + random() * 2, (pos_y / scale) + random() * 2,
+                                                   ((pos_x / scale) + 2) + random() * 2,
+                                                   ((pos_y / scale) + 2) + random() * 2, fill="gray", outline="")
+
+                            # TODO: check if we need this logic or just set static sleep. maybe 1/q*10?
+                            # print("sleep:" + str(abs(0.1 / max(diff_pos))))
+                            # max_diff = 1 if max(diff_pos) == 0 else max(diff_pos)
+                            # # print("sleep:" + str(min(abs(0.1 / max_diff), 0.05)))
+                            # time.sleep(min(abs(0.1 / max_diff), random() / 300))
+                            # time.sleep(0.001)
                     et2 = time.time()
                     tt2 = et2 - st2
                     print("[INFO] IF end. Time taken: {:.4f} s".format(tt2))
 
                     # translate positions
-                    pos_x = translate(xmin, pos_x_min, pos_x_max, 0, res_width)
-                    pos_y = translate(ymin, pos_y_min, pos_y_max, 0, res_height)
-                    # pos_x = pos[0]
-                    # pos_y = pos[1]
-
-                    c_pos_x = translate(current_pos[0], pos_x_min, pos_x_max, 0, res_width)
-                    c_pos_y = translate(current_pos[1], pos_y_min, pos_y_max, 0, res_height)
+                    # pos_x = translate(xmin, pos_x_min, pos_x_max, 0, res_width)
+                    # pos_y = translate(ymin, pos_y_min, pos_y_max, 0, res_height)
+                    pos_x = pos[0]
+                    pos_y = pos[1]
 
                     # 14 is the sice of the circle. this offset draws the circle in the middle
-                    self.canva.create_oval((c_pos_x / scale) - 7, (c_pos_y / scale) - 7,
-                                           ((c_pos_x / scale) + 7),
-                                           ((c_pos_y / scale) + 7), fill="white")
+                    self.canva.create_oval((pos_x / scale) - 7, (pos_y / scale) - 7,
+                                           ((pos_x / scale) + 7),
+                                           ((pos_y / scale) + 7), fill="white")
 
-                    utils.draw_bounding_box_on_image_array(img_raw, current_pos[1] / scale, current_pos[0] / scale,
-                                                           (current_pos[1] / scale) + 40, (current_pos[0] / scale) + 40)
+                    utils.draw_bounding_box_on_image_array(img_raw, ymin / scale, xmin / scale, ymax / scale,
+                                                           xmax / scale)
 
                     # update previous position for this index.
-                    prev_poses[i] = current_pos
+                    prev_pos[i] = pos
 
                 et3 = time.time()
                 tt3 = et3 - st3
@@ -237,6 +184,7 @@ class DisplayPhotoEventHandler(FileSystemEventHandler):
                 output_file_path = os.path.join(opt.test_output_path, file_id + '.png')
                 # plt.savefig(output_file_path, bbox_inches='tight', pad_inches=0)
 
+                from PIL import Image
                 im = Image.fromarray(img_raw)
                 im.save(output_file_path)
 
@@ -253,14 +201,12 @@ class DisplayPhotoEventHandler(FileSystemEventHandler):
                 # self.canva.create_oval(5 / scale, 5 / scale, 5 / scale, 5 / scale, fill="black")
 
             except Exception as e:
-                exception_type, exception_object, exception_traceback = sys.exc_info()
-                line_number = exception_traceback.tb_lineno
-                print(line_number, e)
+                print(e)
                 pass
 
             et1 = time.time()
             tt1 = et1 - st1
-            print("[INFO] __________ File change trigger over. Time taken: {:.4f} s __________".format(tt1))
+            print("[INFO] File change trigger over. Time taken: {:.4f} s _______________________".format(tt1))
 
 
 def translate(value, leftMin, leftMax, rightMin, rightMax):
--- webcam.py
+++ webcam.py
@@ -6,7 +6,7 @@ cam = cv2.VideoCapture(0)
 cv2.namedWindow("test")
 img_name = "input/2.jpg"
 time_last_snapshot = 0
-take_shot_every_seconds = 0.5
+take_shot_every_seconds = 0.1
 
 while True:
     # take a snapshot every 5 seconds
@@ -26,7 +26,7 @@ while True:
     if time.perf_counter() - time_last_snapshot > take_shot_every_seconds:
         time_last_snapshot = time.perf_counter()
         cv2.imwrite(img_name, frame)
-        print("{} written!".format(img_name))
+        print("{} written! at {:.4f}".format(img_name, time.time()))
 
 cam.release()
 
